12425cd8115f6750914304f3254a92aa31e630d5
julydsp <julydsp04@gmail.com>
2022-11-13 22:00:59 +0700

    FORM VALIDATION REGISTER

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,9 +3,33 @@ defined('BASEPATH') OR exit('No direct script acces allowed');
 
 class Auth extends CI_Controller 
 {
-
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    
     public function index()
     {
+        $this->load->view('templates/auth_header');
         $this->load->view('Auth/login'); 
+        $this->load->view('templates/auth_footer');
+    }
+    
+    
+    public function registrasi()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'TABUNGAN SISWA | REGISTRASI';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registrasi');
+            $this->load->view('templates/auth_footer');
+        } else {
+             echo 'selamat datang' ;
+        }
+        
     }
 }
---
+++ application/views/Auth/registrasi.php
@@ -0,0 +1,44 @@
+    <div class="container">
+        <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">REGISTRASI</h1>
+                            </div>
+                            <form class="user">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" 
+                                    id="name" name="name" placeholder="Masukkan Nama Lengkap">
+                                </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" 
+                                        id="email" name="email" placeholder="Masukkan Email">
+                                    </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                        id="password1" name="password1" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="password2" name="password2" placeholder="Ulangi Password">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-danger btn-user btn-block">
+                                    Registrasi Akun </button>
+                                </a>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="<?= base_url('/Auth/'); ?> ">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
